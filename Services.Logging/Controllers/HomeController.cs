731fcaa1ce77886816369100f31b7b1da81077d3
mrtkyz <mrtkyz@gmail.com>
2016-02-10 11:22:49 +0200

    commit

--- Services.Logging/Controllers/HomeController.cs
+++ Services.Logging/Controllers/HomeController.cs
@@ -8,7 +8,7 @@ namespace Services.Logging.Controllers
         // GET: api/Home
         public IEnumerable<string> Get()
         {
-            return new string[] { "value1", "value2" };
+            return new string[] { "murat", "orhan" };
         }
 
         // GET: api/Home/5
