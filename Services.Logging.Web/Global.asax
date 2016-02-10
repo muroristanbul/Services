f5f12354115e07a7bc186052faf27b4466cede94
mrtkyz <mrtkyz@gmail.com>
2016-02-10 18:05:55 +0200

    commit

---
+++ Services.Logging.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Services.Logging.Web.MvcApplication" Language="C#" %>
---
+++ Services.Logging.Web/Global.asax.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Routing;
+
+namespace Services.Logging.Web
+{
+    public class MvcApplication : HttpApplication
+    {
+        protected void Application_Start()
+        {
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+        }
+    }
+}
